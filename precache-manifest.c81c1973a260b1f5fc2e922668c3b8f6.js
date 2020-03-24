0b39de18789b7dbdc7de941beacb83a8ff4a74f9
Jose Luis Cahuana <joseluis.cahuana@hotmail.com>
2020-03-24 14:29:35 -0500

    Updates

---
+++ precache-manifest.c81c1973a260b1f5fc2e922668c3b8f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03070e80320c86e0c81ee5536e8c97e2",
+    "url": "/bebidas/index.html"
+  },
+  {
+    "revision": "16f42b38cd5b002e3f3f",
+    "url": "/bebidas/static/css/main.14805593.chunk.css"
+  },
+  {
+    "revision": "9c19adc7232634d2cd71",
+    "url": "/bebidas/static/js/2.a4e0a0d8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bebidas/static/js/2.a4e0a0d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16f42b38cd5b002e3f3f",
+    "url": "/bebidas/static/js/main.fbbba2a5.chunk.js"
+  },
+  {
+    "revision": "6960e90085059de88714",
+    "url": "/bebidas/static/js/runtime-main.403e12a6.js"
+  }
+]);
